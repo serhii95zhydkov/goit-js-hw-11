2eb91cc7a1795868021a4280d4e5110a52990105
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-12-04 22:42:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './css/styles.css';
+import "./css/styles.css";
+
+import { Notify } from "notiflix";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
