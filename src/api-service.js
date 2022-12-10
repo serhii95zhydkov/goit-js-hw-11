882b0fcdc59badcb3eed58696eb976c328dd41c4
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-12-10 23:05:34 +0200

    Update api-service.js

--- src/api-service.js
+++ src/api-service.js
@@ -1,40 +1,45 @@
 export default class ApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
   }
 
   fetchImages() {
-
     const URL = 'https://pixabay.com/api/';
 
     const API_KEY = '31968153-ea36afa9c46b62a3fbea8fe58';
 
     const ADD_PARAMETERS =
-        'image_type=photo&orientation=horizontal&safesearch=true';
-      
-    
-    return fetch(`${URL}?key=${API_KEY}&q=${this.searchQuery}&${ADD_PARAMETERS}&page=${this.page}&per_page=40`)
-    .then(r => r.json())
-    .then(data => {
+      'image_type=photo&orientation=horizontal&safesearch=true';
+
+    return fetch(
+      `${URL}?key=${API_KEY}&q=${this.searchQuery}&${ADD_PARAMETERS}&page=${this.page}&per_page=40`
+    )
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(({ hits }) => {
         this.incrementPage();
-        return data.hits;
-    });
-    }
+        return hits;
+      });
+  }
 
-    incrementPage() {
-        this.page += 1;
-    }
+  incrementPage() {
+    this.page += 1;
+  }
 
-    resetPage() {
-        this.page = 1;
-    }
-    
-    get query() {
-        return this.searchQuery;
-    }
+  resetPage() {
+    this.page = 1;
+  }
 
-    set query(newQuery) {
-        this.searchQuery = newQuery;
-    }
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 };
