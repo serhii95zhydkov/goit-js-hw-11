5dd4f38a7ddb53cde369fa80e2f40af8d07ae0fc
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-12-10 12:29:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,26 @@ import { Notify } from "notiflix";
 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const URL = "https://pixabay.com/api/";
+
+const API_KEY = "31968153-ea36afa9c46b62a3fbea8fe58";
+
+const ADD_PARAMETERS = "image_type=photo&orientation=horizontal&safesearch=true";
+
+const refs = {
+    form: document.querySelector("#search-form"),
+}
+
+refs.form.addEventListener("submit", onSearchImages);
+
+function onSearchImages(event) {
+    event.preventDefault();
+
+    const searchImages = event.currentTarget.elements.searchQuery.value;
+    console.log(searchImages);
+
+    fetch(`${URL}?key=${API_KEY}&q=${searchImages}&${ADD_PARAMETERS}&page=1&per_page=40`)
+    .then(r => r.json())
+    .then(console.log);
+};
