ec91b8e403549ee369d9ded212b85684145c9901
serhii95zhydkov <s.o.zhidkov@gmail.com>
2022-12-10 13:19:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-import "./css/styles.css";
+import './css/styles.css';
 
-import { Notify } from "notiflix";
+import ApiService from './api-service';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import { Notify } from 'notiflix';
 
-const URL = "https://pixabay.com/api/";
-
-const API_KEY = "31968153-ea36afa9c46b62a3fbea8fe58";
-
-const ADD_PARAMETERS = "image_type=photo&orientation=horizontal&safesearch=true";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
-    form: document.querySelector("#search-form"),
-}
+  form: document.querySelector('#search-form'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
 
-refs.form.addEventListener("submit", onSearchImages);
+refs.form.addEventListener('submit', onSearchImages);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+const apiService = new ApiService();
+
 
 function onSearchImages(event) {
     event.preventDefault();
 
-    const searchImages = event.currentTarget.elements.searchQuery.value;
-    console.log(searchImages);
+    apiService.query = event.currentTarget.elements.searchQuery.value;
+    
+    apiService.fetchImages();
+}
 
-    fetch(`${URL}?key=${API_KEY}&q=${searchImages}&${ADD_PARAMETERS}&page=1&per_page=40`)
-    .then(r => r.json())
-    .then(console.log);
-};
+function onLoadMore() {
+    apiService.fetchImages();
+}
